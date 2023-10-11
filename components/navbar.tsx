09d5a292d104df6da0c24e5535be45186fce4d59
Denys Borysiuk <denisborisiuk@gmail.com>
2023-10-12 01:52:39 +0300

    add main-nav & types

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,19 @@
+import Link from 'next/link';
+
 import Container from '@/components/ui/container';
+import MainNav from '@/components/main-nav';
 
 const Navbar = () => {
   return (
     <div className="border-b">
-      <Container>Navbar!</Container>
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/">
+            <p className="font-bold text-xl">STORE</p>
+          </Link>
+          <MainNav data={[]} />
+        </div>
+      </Container>
     </div>
   );
 };
