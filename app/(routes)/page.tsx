0cb5ec98f16554fddfb3863a5634de66f9350c8a
Denys Borysiuk <denisborisiuk@gmail.com>
2023-10-12 22:53:57 +0300

    add billboard component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,16 @@
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
 const HomePage = () => {
-  return <div>Hello Store</div>;
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
