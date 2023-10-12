f7881408c588b97d253db1fbf1a51d6736f87052
Denys Borysiuk <denisborisiuk@gmail.com>
2023-10-12 23:08:05 +0300

    finish billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,15 @@
+import getBillboard from '@/actions/get-billboard';
 import Billboard from '@/components/billboard';
 import Container from '@/components/ui/container';
 
 export const revalidate = 0;
 
-const HomePage = () => {
+const HomePage = async () => {
+  const billboard = await getBillboard('112c67f8-1a85-4596-97b4-9976560415d1');
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard />
+        <Billboard data={billboard} />
       </div>
     </Container>
   );
